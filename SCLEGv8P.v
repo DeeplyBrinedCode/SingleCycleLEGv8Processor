9165cce33792fdacdb06cae513cbfa96402687d0
DeeplyBrinedCode <135945715+DeeplyBrinedCode@users.noreply.github.com>
2026-06-06 12:57:12 -0700

    Add files via upload

--- SCLEGv8P.v
+++ SCLEGv8P.v
@@ -3,7 +3,7 @@ module SCLEGv8P (
 	);
 	
 	reg [31:0] PC, Instruction, ReadData1, ReadData2, Immediate, ALUResult, MemData;
-	wire [31:0] PC_wire, Instruction_wire;
+	wire [31:0] PC_wire, Instruction_wire, RD1_wire, RD2_wire;
 	
 	assign PC_wire = PC;
 	
@@ -16,7 +16,21 @@ module SCLEGv8P (
 	//----------------------------------------------------------------------------------
 	// Register File
 	//----------------------------------------------------------------------------------
-
+	
+	reg [4:0] RF_ReadReg1, RF_ReadReg2, RF_WriteReg;
+	reg [31:0] RF_WriteData;
+	wire [31:0] RF_ReadData1, RF_ReadData2;
+	reg RF_WriteCmd;
+	
+	RegisterFile RF (
+		RF_ReadReg1, RF_ReadReg2, RF_WriteReg, RF_WriteData,
+		RF_WriteCmd, RF_ReadData1, RF_ReadData2
+	);
+	
+	//----------------------------------------------------------------------------------
+	// ALU
+	//----------------------------------------------------------------------------------
+	
 	
 	//----------------------------------------------------------------------------------
 	// Register Update
@@ -36,6 +50,25 @@ module SCLEGv8P (
 			default: begin
 				PC <= PC + 32'h00000100;
 				Instruction <= Instruction_wire;
+				ReadData1 <= RF_ReadData1;
+				ReadData2 <= RF_ReadData2;
+			end
+		endcase
+		// Check opcode
+		case(Instruction[31:20])
+			12'h458, 12'h658, : begin // R-Format Opcodes
+				RF_ReadReg1 <= Instruction[9:5]; // First Source Register
+				RF_ReadReg2 <= Instruction[20:16]; // Second Source Register
+				RF_WriteReg <= Instruction[4:0]; // Destination Register
+				RF_WriteData <= ALUResult;
+				RF_WriteCmd <= 1'b1;
+			end
+			default: begin
+				RF_ReadReg1 <= 5'bzzzzz;
+				RF_ReadReg2 <= 5'bzzzzz;
+				RF_WriteReg <= 5'bzzzzz;
+				RF_WriteData <= 32'hzzzzzzzz;
+				RF_WriteCmd <= 1'bz;
 			end
 		endcase
 	end
