890fb24f0cecaa2a1a5795ef7183d3404491bd3d
rschwarz1 <rschwarz1@seattleu.edu>
2026-06-08 20:14:43 -0700

    Update ALU.v
    ALU_result

--- ALU.v
+++ ALU.v
@@ -3,16 +3,16 @@ module ArLoUn  (
 	input [4:0] Ctrl,
 	input [31:0] oppA, oppB,
 	output reg Z,
-	output reg[31:0] result
+	output reg[31:0] ALU_Result
 	);
 	
 	always @(*) begin
 		case(Ctrl)
-			5'b00000:	result = oppA + oppB;
-			5'b00001:	result = oppA - oppB;
-			5'b00010:	result = oppA & oppB;
-			5'b00011: 	result = oppA | oppB;
-			5'b00100:	result = !(oppA & oppB);
+			5'b00000:	ALU_Result = oppA + oppB;
+			5'b00001:	ALU_Result = oppA - oppB;
+			5'b00010:	ALU_Result = oppA & oppB;
+			5'b00011: 	ALU_Result = oppA | oppB;
+			5'b00100:	ALU_Result = !(oppA & oppB);
 			default: result = 0;
 		endcase
 	if (result == 0) 
