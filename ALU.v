d1e394608afc4f6644800b067296fc0b2b75ab37
DeeplyBrinedCode <135945715+DeeplyBrinedCode@users.noreply.github.com>
2026-06-08 22:10:45 -0700

    Add files via upload

--- ALU.v
+++ ALU.v
@@ -1,6 +1,5 @@
-
 module ArLoUn  (
-	input [4:0] Ctrl,
+	input [10:0] Ctrl,
 	input [31:0] oppA, oppB,
 	output reg Z,
 	output reg[31:0] ALU_Result
@@ -8,14 +7,14 @@ module ArLoUn  (
 	
 	always @(*) begin
 		case(Ctrl)
-			5'b00000:	ALU_Result = oppA + oppB;
-			5'b00001:	ALU_Result = oppA - oppB;
-			5'b00010:	ALU_Result = oppA & oppB;
-			5'b00011: 	ALU_Result = oppA | oppB;
-			5'b00100:	ALU_Result = !(oppA & oppB);
+			11'h458, 11'h488, 11'h489:	ALU_Result = oppA + oppB;
+			11'h658, 11'h688, 11'h689:	ALU_Result = oppA - oppB;
+			11'h450:	ALU_Result = oppA & oppB;
+			11'h550: ALU_Result = oppA | oppB;
+			11'h4bc:	ALU_Result = !(oppA & oppB); //11'h4bc is the NAND opcode
 			default: ALU_Result = 0;
 		endcase
-	if (ALU_Result == 0) 
-		Z = 1'b1;
+		if (ALU_Result == 0) 
+			Z = 1'b1;
 	end
 endmodule
